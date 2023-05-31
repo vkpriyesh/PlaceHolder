ff4abdcfa9c11fcc03c34b0702db42b62bd7f266
Working Rom <dpriyeshvk@live.co.uk>
2023-05-31 18:58:24 +0800

    Update DscStuff.ps1

--- LotsOfBiceps/Most/DscStuff.ps1
+++ LotsOfBiceps/Most/DscStuff.ps1
@@ -45,3 +45,24 @@ $nodeId = (Get-AzAutomationDscNode -ResourceGroupName $resourceGroupName -Automa
 
 # Start the DSC configuration deployment
 Start-AzAutomationDscNodeConfigurationDeployment -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -NodeConfigurationName $configurationName -NodeId $nodeId
+# Parameters
+param(
+    [string]$resourceGroupName,
+    [string]$automationAccountName,
+    [string]$configurationName,
+    [string]$configurationDataPath
+)
+
+# Define the DSC configuration
+$configurationContent = Get-Content -Path 'path-to-your-dsc-file' -Raw
+Invoke-Expression -Command $configurationContent
+
+# Export the configuration to a .mof file
+$configurationName -OutputPath '.\'
+
+# Import the DSC configuration
+Import-AzAutomationDscConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -SourcePath ".\$configurationName.ps1" -Published -Force
+
+# Compile the DSC configuration
+$compilationJob = Start-AzAutomationDscCompilationJob -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ConfigurationName $configurationName -ConfigurationDataPath $configurationDataPath
+$compilationJob | Wait-AzAutomationDscCompilationJob
