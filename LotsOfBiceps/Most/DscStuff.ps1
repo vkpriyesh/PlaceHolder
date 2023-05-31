bc19ea7b98594dccdedfc080b6fc375c73780325
Working Rom <dpriyeshvk@live.co.uk>
2023-05-31 18:39:41 +0800

    Create DscStuff.ps1

---
+++ LotsOfBiceps/Most/DscStuff.ps1
@@ -0,0 +1,47 @@
+# Parameters
+param(
+    [string]$resourceGroupName,
+    [string]$automationAccountName,
+    [string]$configurationName,
+    [string]$configurationPath,
+    [string]$configurationDataPath
+)
+
+# Import the DSC configuration
+Import-AzAutomationDscConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -SourcePath $configurationPath -Published -Force
+
+# Compile the DSC configuration
+$compilationJob = Start-AzAutomationDscCompilationJob -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ConfigurationName $configurationName -ConfigurationDataPath $configurationDataPath
+$compilationJob | Wait-AzAutomationDscCompilationJob
+# Parameters
+param(
+    [string]$subscriptionId,
+    [string]$resourceGroupName,
+    [string]$automationAccountName,
+    [string]$vmName,
+    [string]$configurationName,
+    [string]$configurationPath,
+    [string]$configurationDataPath
+)
+
+# Login to Azure
+Connect-AzAccount
+
+# Select the correct subscription
+Set-AzContext -SubscriptionId $subscriptionId
+
+# Register the VM as DSC Node
+Register-AzAutomationDscNode -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -AzureVMName $vmName -NodeConfigurationName $configurationName
+
+# Import the configuration
+Import-AzAutomationDscConfiguration -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -SourcePath $configurationPath -Published -Force
+
+# Compile the DSC configuration
+$compilationJob = Start-AzAutomationDscCompilationJob -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ConfigurationName $configurationName -ConfigurationDataPath $configurationDataPath
+$compilationJob | Wait-AzAutomationDscCompilationJob
+
+# Get Node Id of the VM
+$nodeId = (Get-AzAutomationDscNode -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName | Where-Object {$_.Name -eq $vmName}).Id
+
+# Start the DSC configuration deployment
+Start-AzAutomationDscNodeConfigurationDeployment -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -NodeConfigurationName $configurationName -NodeId $nodeId
