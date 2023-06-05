03dd079c3e3061525995de2f0330c564ca21969f
Working Rom <dpriyeshvk@live.co.uk>
2023-06-05 18:47:03 +0800

    Update 1DscStuff.ps1

--- LotsOfBiceps/Most/1DscStuff.ps1
+++ LotsOfBiceps/Most/1DscStuff.ps1
@@ -34,3 +34,30 @@ Register-AzAutomationDscNode -ResourceGroupName $resourceGroupName -AutomationAc
 
 # Apply the DSC configuration to the Azure VM
 Start-AzAutomationDscCompilationJob -ResourceGroupName $resourceGroupName -AutomationAccountName $automationAccountName -ConfigurationName $nodeConfigurationName
+# Import the required modules
+Import-Module Az.Accounts
+Import-Module Az.Automation
+
+# Login to Azure account
+$credential = Get-Credential
+Connect-AzAccount -Credential $credential
+
+# Select the subscription
+$subscriptionId = 'Your-Azure-Subscription-Id'
+Set-AzContext -SubscriptionId $subscriptionId
+
+# Define hashtable containing module names and URLs
+$modules = @{
+    ModuleName1 = 'ModuleUrl1'
+    ModuleName2 = 'ModuleUrl2'
+}
+
+# Define automation account and resource group
+$resourceGroupName = 'Your-Azure-Resource-Group'
+$automationAccountName = 'Your-Automation-Account'
+
+# Import the modules
+foreach($moduleName in $modules.Keys) {
+    $moduleUrl = $modules[$moduleName]
+    New-AzAutomationModule -AutomationAccountName $automationAccountName -ResourceGroupName $resourceGroupName -ContentLinkUri $moduleUrl -Name $moduleName
+}
